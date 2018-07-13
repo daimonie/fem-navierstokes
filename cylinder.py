68dc2b18658263f8d31745e9f2783e129abb06e4
daimonie <deboer@lorentz.leidenuniv.nl>
2018-07-13 12:10:59 +0200

    Attempted to fix it. It is almost working, soon(tm).

--- cylinder.py
+++ cylinder.py
@@ -1,46 +1,54 @@
 import sys;  
-from simple_navier_stokes import *; 
+from simple_navier_stokes import simple_navier_stokes, cylinder; 
 import numpy as np;
 import argparse as argparse;
 import matplotlib.pyplot as plt;
 import time as time; 
 
-startTime = time.time();
+start_time = time.time();
 
 file_name = "data/cylinder/";
  
-sns = sns.cylinder.cylinder (file_name);
+sns = cylinder.cylinder(file_name);
 
 parser = argparse.ArgumentParser(prog="python cylinder.py",
   description = "Initialises and configures a simple navier stokes solver for the 2D karman sheet.");
 
-parser.add_argument('--finalTime', '-T', help='Final Time of the program.', type=float, default=0.01)
+parser.add_argument('--final_time', '-T', help='Final Time of the program.', type=float, default=0.01)
 parser.add_argument('--numberSteps', '-N', help='Number of steps the program iterates.', type=int, default=1000)
-parser.add_argument('--dynamicViscosity', '-V', help='Fluid dynamic viscosity.', type=float, default=sns.finalTime)
+parser.add_argument('--dynamicViscosity', '-V', help='Fluid dynamic viscosity.', type=float, default=sns.final_time)
 parser.add_argument('--density', '-D', help='Fluid mass density.', type=float, default=sns.density)
-parser.add_argument('--meshFineness', '-M', help='Fineness of the mesh.', type=int, default=sns.meshFineness)
+parser.add_argument('--meshFineness', '-M', help='Fineness of the mesh.', type=int, default=sns.mesh_fineness)
 parser.add_argument('--visualization', '-S', help='Show visualization? Slows down excecution..', type=bool, default=True)
-parser.add_argument('--animationInterval', '-A', help='How often the visualization is updated..', type=int, default=sns.animationInterval)
+parser.add_argument('--animationInterval', '-A', help='How often the visualization is updated..', type=int, default=sns.animation_interval)
 args = parser.parse_args();
 
-sns.finalTime			= args.finalTime;
-sns.numberTimeSteps	    = args.numberSteps;
-sns.deltaTime 			= sns.finalTime / sns.numberTimeSteps;
-sns.dynamicViscosity	= args.dynamicViscosity;
+sns.final_time			= args.final_time;
+sns.number_time_steps	= args.numberSteps;
+sns.delta_time 			= args.final_time / args.numberSteps;
+sns.dynamic_viscosity	= args.dynamicViscosity;
 sns.density 			= args.density; 
-sns.meshFineness 		=  args.meshFineness;
-sns.visualization 		=  args.visualization;
-sns.animationInterval 	=  args.animationInterval;
+sns.mesh_fineness 		= args.meshFineness;
+sns.visualization 		= args.visualization;
+sns.animation_interval 	= args.animationInterval;
 try:
 	print "PreProcessing Simple NavierStokes. \n";
-	sns.preProcess ();
+	sns.pre_process ();
 	
 	sns.show();
 except AssertionError:
 	print "Assertion Error was thrown. Likely, simple_navier_stokes was not preprocessed. \n";
+	plt.close()
+	sys.exit()
 except AttributeError:
 	print "Attribute Error thrown. This is something related to closing the window; idc. \n";
+	plt.close()
+	sys.exit()
+except TypeError:
+	print "TypeError Error thrown. Probably means the code is wrong somehow. Bai. \n";
+	plt.close()
+	sys.exit()
 
-finalTime = time.time ();
+final_time = time.time ();
 
-print "Process took %.3f seconds." % ( finalTime - startTime);
+print "Process took %.3f seconds." % ( final_time - start_time);
--- simple_navier_stokes/cylinder.py
+++ simple_navier_stokes/cylinder.py
@@ -4,31 +4,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation 
 from matplotlib.ticker import NullFormatter
-from fenics import *;
-from mshr import *;
-import sys as sys;
+from fenics import *
+from mshr import *
+import sys as sys
+from simple_navier_stokes import *
 
 class cylinder(simple_navier_stokes):
     def __init__(self, file_name): 
-        simple_navier_stokes.__init__(self, file_name);
+        simple_navier_stokes.__init__(self, file_name)
 
     def create_mesh(self):
         # Create mesh
-        channel = Rectangle(Point(0, 0), Point(2.2, 0.41));
-        cylinder = Circle(Point(0.2, 0.2), 0.05);
+        channel = Rectangle(Point(0, 0), Point(2.2, 0.41))
+        cylinder = Circle(Point(0.2, 0.2), 0.05)
 
 
-        domain = channel - cylinder;
+        domain = channel - cylinder
 
-        self.mesh = generate_mesh(domain, self.mesh_fineness);
+        self.mesh = generate_mesh(domain, self.mesh_fineness)
     def create_inflow_profile (self):
-        self.inflow_profile_expression = ('4.0*1.5*x[1]*(0.41 - x[1]) / pow(0.41, 2)', '0');
+        self.inflow_profile_expression = ('4.0*1.5*x[1]*(0.41 - x[1]) / pow(0.41, 2)', '0')
     def create_boundaries(self):
-        boundary_inflow_expression   = 'near(x[0], 0)';
-        boundary_walls_expression    = 'near(x[1], 0) || near(x[1], 0.41)';
-        boundary_cylinder_expression = 'on_boundary && x[0]>0.1 && x[0]<0.3 && x[1]>0.1 && x[1]<0.3';
+        boundary_inflow_expression   = 'near(x[0], 0)'
+        boundary_walls_expression    = 'near(x[1], 0) || near(x[1], 0.41)'
+        boundary_cylinder_expression = 'on_boundary && x[0]>0.1 && x[0]<0.3 && x[1]>0.1 && x[1]<0.3'
 
-        boundary_outflow_expression  = 'near(x[0], 2.2)';
+        boundary_outflow_expression  = 'near(x[0], 2.2)'
 
         boundary_inflow = DirichletBC(self.velocity_function_space, Expression(self.inflow_profile_expression, degree=2), boundary_inflow_expression)
         boundary_walls = DirichletBC(self.velocity_function_space, Constant((0, 0)), boundary_walls_expression)
@@ -36,7 +37,7 @@ class cylinder(simple_navier_stokes):
         boundary_outflow = DirichletBC(self.pressure_function_space, Constant(0), boundary_outflow_expression)
 
 
-        self.boundary_conditions_velocity = [boundary_inflow, boundary_walls, boundary_cylinder];
+        self.boundary_conditions_velocity = [boundary_inflow, boundary_walls, boundary_cylinder]
         self.boundary_conditions_pressure = [boundary_outflow];
 
         self.boundary_conditions_test = 'near(x[0], 0)';
